002013bfc67ec7be5bb65553ab03e65e22e140f3
Yiwen Wang 🌊 <yiwwan@microsoft.com>
2021-08-20 12:03:43 +0800

    Initial commit

---
+++ sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.Variables.json")
+    .AddJsonFile("appsettings.json")
+    .Build()
+    .ResolveReferences();
+
+Console.WriteLine($"Value of Key1: {configuration["Key1"]}");
+Console.WriteLine($"Value of Key2: {configuration["Key2"]}");
+Console.WriteLine($"Value of Key3: {configuration["Key3"]}");
+Console.WriteLine($"Value of non-existing key Key4: {configuration["Key4"]}");
---
+++ src/IConfigurationReferencingExtensions.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.Extensions.Configuration
+{
+    public static class IConfigurationReferencingExtensions
+    {
+        private static readonly Regex ReferenceMatchingPattern = new Regex(@"\$\(([\w:]+?)\)");
+
+        public static IConfiguration ResolveReferences(this IConfiguration configuration)
+        {
+            foreach (var kvp in configuration.AsEnumerable())
+            {
+                if (kvp.Value == null)
+                {
+                    continue;
+                }
+                var match = ReferenceMatchingPattern.Match(kvp.Value);
+                if (match.Success)
+                {
+                    var configPath = match.Groups[1].Value;
+                    try
+                    {
+                        configuration[kvp.Key] = configuration[configPath];
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        if (configuration is IConfigurationRoot configurationRoot)
+                        {
+                            foreach (var provider in configurationRoot.Providers)
+                            {
+                                try
+                                {
+                                    provider.Set(kvp.Key, configuration[configPath]);
+                                }
+                                catch (InvalidOperationException)
+                                {
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return configuration;
+        }
+    }
+}
